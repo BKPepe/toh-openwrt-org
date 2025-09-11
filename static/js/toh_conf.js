263871b6bdc7012b44429da6ad2e8c75112fad8c
soif <wxopwx@gmail.com>
2025-09-11 23:51:40 +0200

    version 1.79b2

--- static/js/toh_conf.js
+++ static/js/toh_conf.js
@@ -4,7 +4,7 @@
 
 // global -------------------------------------------------------
 const toh_app={
-	version:	"1.79b1",	// Version
+	version:	"1.79b2",	// Version
 	branch:		"dev", 		// Branch, either: 'prod' | 'dev'	
 };
 
